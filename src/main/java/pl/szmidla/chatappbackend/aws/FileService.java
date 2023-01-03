a58621971f677c6d875a8eacd92dd34b0b7c148d
TheYGD <jakub.szmidla@o2.pl>
2023-01-03 01:24:27 +0100

    Connected application with AWS
    Created AWS Config class that establishes connection with AWS account
    and gives S3Client Bean.
    Also created FileService. This service runs all the requests to the S3
    bucket.
    Also created config file for Mockito, which makes it possible to mock
    final classes, which is necessary for testing the AWS final classes.

---
+++ src/main/java/pl/szmidla/chatappbackend/aws/FileService.java
@@ -0,0 +1,65 @@
+package pl.szmidla.chatappbackend.aws;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FileService {
+
+    @Value("${aws.bucket}")
+    private String bucketName;
+    private final S3Client s3;
+
+
+    public byte[] download(String pathFileName) throws IOException {
+        GetObjectRequest objectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(pathFileName)
+                .build();
+
+        try {
+            return s3.getObject(objectRequest).readAllBytes();
+        } catch (IOException e) {
+            log.error("Failed to download file from s3, pathFileName={}", pathFileName);
+            throw e;
+        }
+    }
+
+    public void save(String pathFileName,
+                     Map<String, String> metadata, InputStream inputStream) throws IOException {
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(pathFileName)
+                .metadata(metadata != null ? metadata : Map.of())
+                .build();
+
+        try {
+            s3.putObject(objectRequest, RequestBody.fromBytes(inputStream.readAllBytes()));
+        } catch (IOException e) {
+            log.error("Failed to save file to s3, pathFileName={}", pathFileName);
+            throw e;
+        }
+    }
+
+    public void remove(String pathFileName) {
+        DeleteObjectRequest objectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(pathFileName)
+                .build();
+        s3.deleteObject(objectRequest);
+    }
+}
