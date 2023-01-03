8805171ce194f5d21273a869656ff51d35e1d359
TheYGD <jakub.szmidla@o2.pl>
2023-01-03 01:47:17 +0100

    Addition to previous commit

--- src/main/java/pl/szmidla/chatappbackend/data/dto/UserResponse.java
+++ src/main/java/pl/szmidla/chatappbackend/data/dto/UserResponse.java
@@ -10,11 +10,13 @@ public class UserResponse {
 
     private long id;
     private String username;
+    private String imageUrl;
 
     public static UserResponse fromUser(User user) {
         UserResponse userResponse = new UserResponse();
         userResponse.setId(user.getId());
         userResponse.setUsername(user.getUsername());
+        userResponse.setImageUrl(user.getImageUrl());
         return userResponse;
     }
 }
